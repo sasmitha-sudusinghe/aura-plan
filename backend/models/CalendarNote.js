ee50fc6245c8c05cc5f52633f9cabd0b4a6d80bd
sasmitha-sudusinghe <sasmithasudusinghe@gmail.com>
2026-05-24 21:18:24 +0530

    chore: remove backend and client folders, keep only static root files

--- backend/models/CalendarNote.js
+++
@@ -1,9 +0,0 @@
-// models/CalendarNote.js
-const mongoose = require('mongoose');
-
-const CalendarNoteSchema = new mongoose.Schema({
-  date: { type: String, required: true, unique: true }, // Format "YYYY-MM-DD"
-  notes: { type: String, default: '' }
-});
-
-module.exports = mongoose.model('CalendarNote', CalendarNoteSchema);
